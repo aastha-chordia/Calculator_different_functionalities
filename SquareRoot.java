b9ca773d0e7bae62d446597e296b5ea2bde7c2a1
Aastha Chordia <aasthachordia595@gmail.com>
2025-04-19 01:00:21 +0530

    return with the final output

--- SquareRoot.java
+++ SquareRoot.java
@@ -3,4 +3,7 @@ public class SquareRoot implements CalculatorOperation {
     public double calculate(double a, double b) throws IllegalArgumentException {
         if (a < 0) {
             throw new IllegalArgumentException("Cannot calculate square root of a negative number");
-       
+        }
+        return Math.sqrt(a);
+    }
+}
