920bf3aed724520e08df2f90c5e1da536c9b8302
Aastha Chordia <aasthachordia595@gmail.com>
2025-04-19 01:00:00 +0530

    if statement with throws

--- SquareRoot.java
+++ SquareRoot.java
@@ -1,4 +1,6 @@
 public class SquareRoot implements CalculatorOperation {
     
     public double calculate(double a, double b) throws IllegalArgumentException {
-        
+        if (a < 0) {
+            throw new IllegalArgumentException("Cannot calculate square root of a negative number");
+       
