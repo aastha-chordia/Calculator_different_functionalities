6cb797187e3c6f68f894b69043dfa2c5b9b5112f
Aastha Chordia <aasthachordia595@gmail.com>
2025-04-19 00:32:16 +0530

    Update Main.java
    1st print statement

--- Main.java
+++ Main.java
@@ -6,3 +6,12 @@ Batch: Aiml A1
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        CalculatorOperations calculator = new CalculatorOperations();
+        
+        System.out.println("Java Calculator");
+    }
+}
