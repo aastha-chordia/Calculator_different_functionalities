e84b650745e8b1d07b2024ae706f75e780b3d866
Aastha Chordia <aasthachordia595@gmail.com>
2025-04-19 00:33:50 +0530

    Update Main.java
    list of functions

--- Main.java
+++ Main.java
@@ -15,6 +15,18 @@ public class Main {
         System.out.println("Java Calculator");
          
         while (true) {
-            }
+            
+            // Display menu
+            System.out.println("\nAvailable Operations:");
+            System.out.println("1. Addition");
+            System.out.println("2. Subtraction");
+            System.out.println("3. Multiplication");
+            System.out.println("4. Division");
+            System.out.println("5. Square");
+            System.out.println("6. Cube");
+            System.out.println("7. Square Root");
+            System.out.println("8. Exit");
+            
+           
         }
     }
