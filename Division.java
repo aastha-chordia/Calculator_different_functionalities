348f8cc9c9519b0a55413b950701152c02b3f188
Aastha Chordia <aasthachordia595@gmail.com>
2025-04-19 00:55:12 +0530

    exception

--- Division.java
+++ Division.java
@@ -2,4 +2,5 @@ public class Division implements CalculatorOperation {
    
     public double calculate(double a, double b) throws ArithmeticException {
         if (b == 0) {
-          
+            throw new ArithmeticException("Division by zero is not allowed");
+       
